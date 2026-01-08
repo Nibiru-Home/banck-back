010877622c707a02b3220e8fafd9f6b5b8296a9d
R4r4s <rngheru@gmail.com>
2026-01-08 09:13:50 +0100

    Añadir Proyecto

---
+++ src/main/java/banck_back/banck_back/BanckBackApplication.java
@@ -0,0 +1,13 @@
+package banck_back.banck_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanckBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanckBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/banck_back/banck_back/BanckBackApplicationTests.java
@@ -0,0 +1,13 @@
+package banck_back.banck_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanckBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
