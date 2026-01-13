3674fd16e1bb64e74537511c4a826fd0ad8c9f8b
ainhoa <ainhoamiret@gmail.com>
2026-01-13 12:47:32 +0100

    feat(Nibiru-Home/sprints#39): Config

---
+++ src/main/java/bank_back/bank_back/config/SpringConfig.java
@@ -0,0 +1,99 @@
+package bank_back.bank_back.config;
+
+import bank_back.bank_back.domain.repository.BankAccountRepository;
+import bank_back.bank_back.domain.repository.BankMovementRepository;
+import bank_back.bank_back.domain.repository.ClientRepository;
+import bank_back.bank_back.domain.repository.CreditCardRepository;
+import bank_back.bank_back.domain.service.BankAccountService;
+import bank_back.bank_back.domain.service.BankMovementService;
+import bank_back.bank_back.domain.service.ClientService;
+import bank_back.bank_back.domain.service.CreditCardService;
+import bank_back.bank_back.domain.service.impl.BankAccountServiceImpl;
+import bank_back.bank_back.domain.service.impl.BankMovementServiceImpl;
+import bank_back.bank_back.domain.service.impl.ClientServiceImpl;
+import bank_back.bank_back.domain.service.impl.CreditCardServiceImpl;
+import bank_back.bank_back.persistence.dao.jpa.BankAccountJpaDao;
+import bank_back.bank_back.persistence.dao.jpa.BankMovementJpaDao;
+import bank_back.bank_back.persistence.dao.jpa.ClientJpaDao;
+import bank_back.bank_back.persistence.dao.jpa.CreditCardJpaDao;
+import bank_back.bank_back.persistence.dao.jpa.impl.BankAccountJpaDaoImpl;
+import bank_back.bank_back.persistence.dao.jpa.impl.BankMovementJpaDaoImpl;
+import bank_back.bank_back.persistence.dao.jpa.impl.ClientJpaDaoImpl;
+import bank_back.bank_back.persistence.dao.jpa.impl.CreditCardJpaDaoImpl;
+import bank_back.bank_back.persistence.repository.impl.BankAccountRepositoryImpl;
+import bank_back.bank_back.persistence.repository.impl.BankMovementRepositoryImpl;
+import bank_back.bank_back.persistence.repository.impl.ClientRepositoryImpl;
+import bank_back.bank_back.persistence.repository.impl.CreditCardRepositoryImpl;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "bank_back.bank_back.persistence.dao.jpa")
+@EntityScan(basePackages = "bank_back.bank_back.persistence.dao.jpa.entity")
+public class SpringConfig {
+
+    // JPA DAOs
+    @Bean
+    public BankAccountJpaDao bankAccountJpaDao() {
+        return new BankAccountJpaDaoImpl();
+    }
+
+    @Bean
+    public BankMovementJpaDao bankMovementJpaDao() {
+        return new BankMovementJpaDaoImpl();
+    }
+
+    @Bean
+    public ClientJpaDao clientJpaDao() {
+        return new ClientJpaDaoImpl();
+    }
+
+    @Bean
+    public CreditCardJpaDao creditCardJpaDao() {
+        return new CreditCardJpaDaoImpl();
+    }
+
+    // Repositories
+    @Bean
+    public BankAccountRepository bankAccountRepository(BankAccountJpaDao bankAccountJpaDao) {
+        return new BankAccountRepositoryImpl(bankAccountJpaDao);
+    }
+
+    @Bean
+    public BankMovementRepository bankMovementRepository(BankMovementJpaDao bankMovementJpaDao) {
+        return new BankMovementRepositoryImpl(bankMovementJpaDao);
+    }
+
+    @Bean
+    public ClientRepository clientRepository(ClientJpaDao clientJpaDao) {
+        return new ClientRepositoryImpl(clientJpaDao);
+    }
+
+    @Bean
+    public CreditCardRepository creditCardRepository(CreditCardJpaDao creditCardJpaDao) {
+        return new CreditCardRepositoryImpl(creditCardJpaDao);
+    }
+
+    // Services
+    @Bean
+    public BankAccountService bankAccountService(BankAccountRepository bankAccountRepository) {
+        return new BankAccountServiceImpl(bankAccountRepository);
+    }
+
+    @Bean
+    public BankMovementService bankMovementService(BankMovementRepository bankMovementRepository) {
+        return new BankMovementServiceImpl(bankMovementRepository);
+    }
+
+    @Bean
+    public ClientService clientService(ClientRepository clientRepository) {
+        return new ClientServiceImpl(clientRepository);
+    }
+
+    @Bean
+    public CreditCardService creditCardService(CreditCardRepository creditCardRepository) {
+        return new CreditCardServiceImpl(creditCardRepository);
+    }
+}
